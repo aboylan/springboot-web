eb5ab7ec5053c795ba9b634d6bd28bbd2d27317c
Alex Boylan <aboylan@users.noreply.github.com>
2023-12-22 13:39:13 -0600

    Pasar datos a la vista

--- src/main/java/com/aboylan/curso/springboot/webapp/springbootweb/controllers/UserController.java
+++ src/main/java/com/aboylan/curso/springboot/webapp/springbootweb/controllers/UserController.java
@@ -1,14 +1,17 @@
 package com.aboylan.curso.springboot.webapp.springbootweb.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
 
     @GetMapping("/details")
-    public String details() {
-        
+    public String details(Model model) {
+        model.addAttribute("title", "Hola Mundo Spring Boot");
+        model.addAttribute("name", "Alex");
+        model.addAttribute("lastName", "Boylan");
         return "details";
     }
 }
