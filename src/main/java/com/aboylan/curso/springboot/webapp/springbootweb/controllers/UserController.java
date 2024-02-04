16d835c9d8a260439e7ef5f2927486ce2156ea45
Alex Boylan <aboylan@users.noreply.github.com>
2024-02-03 21:33:11 -0600

    Listas en Thymeleaf

--- src/main/java/com/aboylan/curso/springboot/webapp/springbootweb/controllers/UserController.java
+++ src/main/java/com/aboylan/curso/springboot/webapp/springbootweb/controllers/UserController.java
@@ -1,10 +1,16 @@
 package com.aboylan.curso.springboot.webapp.springbootweb.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.aboylan.curso.springboot.webapp.springbootweb.models.User;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class UserController {
@@ -18,4 +24,14 @@ public class UserController {
         model.addAttribute("user", user);
         return "details";
     }
+
+    @GetMapping("/list")
+    public String list(ModelMap model) {
+        List<User> users = new ArrayList<>();
+
+        model.addAttribute("users", users);
+        model.addAttribute("title", "Listado de usaurios!");
+        return "list";
+    }
+    
 }
