6832bb1b7ea0b7ae6bb5ff0ae91d5a928c980aa0
Alex Boylan <aboylan@users.noreply.github.com>
2024-02-05 14:39:29 -0600

    Inyectar valores usando la anotacion @Value

--- src/main/java/com/aboylan/curso/springboot/webapp/springbootweb/controllers/PathVariableController.java
+++ src/main/java/com/aboylan/curso/springboot/webapp/springbootweb/controllers/PathVariableController.java
@@ -3,6 +3,7 @@ package com.aboylan.curso.springboot.webapp.springbootweb.controllers;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,18 @@ import com.aboylan.curso.springboot.webapp.springbootweb.models.dto.ParamDto;
 @RestController
 @RequestMapping("/api/var")
 public class PathVariableController {
+
+    @Value("${config.username}")
+    private String username;
+
+    // @Value("${config.message}")
+    // private String message;
+
+    @Value("${config.listOfValues}")
+    private String[] listOfValues;
+
+    @Value("${config.code}")
+    private Integer code;
 
     @GetMapping("/baz/{message}")
     public ParamDto baz(@PathVariable String message) {
@@ -40,6 +53,17 @@ public class PathVariableController {
         // hacer algo con el usuario save en la bbdd
         user.setName(user.getName().toUpperCase());
         return user;
+    }
+
+    @GetMapping("/values")
+    public Map<String, Object> values(@Value("${config.message}") String message) {
+
+        Map<String, Object> json = new HashMap<>();
+        json.put("username", username);
+        json.put("code", code);
+        json.put("message", message);
+        json.put("listOfValues", listOfValues);
+        return json;
     }
 
 }
