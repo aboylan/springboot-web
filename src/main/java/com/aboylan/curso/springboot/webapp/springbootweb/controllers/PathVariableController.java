5c96f3b6b15196f8c55bcbf125aa7d6afec88b39
Alex Boylan <aboylan@users.noreply.github.com>
2024-02-04 19:29:54 -0600

    Anotacion @PathVariable

---
+++ src/main/java/com/aboylan/curso/springboot/webapp/springbootweb/controllers/PathVariableController.java
@@ -0,0 +1,22 @@
+package com.aboylan.curso.springboot.webapp.springbootweb.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.aboylan.curso.springboot.webapp.springbootweb.models.dto.ParamDto;
+
+@RestController
+@RequestMapping("/api/var")
+public class PathVariableController {
+
+    @GetMapping("/baz/{message}")
+    public ParamDto baz(@PathVariable String message) {
+
+        ParamDto param = new ParamDto();
+        param.setMessage(message);
+        return param;
+    }
+
+}
